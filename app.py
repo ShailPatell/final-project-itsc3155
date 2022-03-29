e13f9d052c98b2639fc3dda8c4ac6667382efb69
Shawn Kiruba <eliezershawn@gmail.com>
2022-03-29 10:14:26 -0400

    Removing .gitignore and adding formatting for html

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('index.html',home_link=True)
+    return render_template('index.html', home_link=True)
+
 
 @app.route('/petfinder')
 def petfinder():
-    return render_template('petfinder.html',adopt_link=True)
+    return render_template('petfinder.html', adopt_link=True)
+
 
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
 
+
 @app.get('/petpost')
 def petpost():
     return render_template('petpost.html', post_link=True)
+
 
 @app.post('/petview')
 def petview():
