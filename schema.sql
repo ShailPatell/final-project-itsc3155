b888456163936708a53bf9379f6af97e126a3b89
Shail Patel <shail.patel.1116@gmail.com>
2022-04-12 14:49:58 -0400

    Pet Schema

--- schema.sql
+++ schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE IF NOT EXISTS user (
     PRIMARY KEY (user_id)
 );
 
+CREATE TABLE IF NOT EXISTS pet (
+    pet_id INT AUTO_INCREMENT NOT NULL,
+    pet_name VARCHAR(255) NOT NULL,
+    pet_age INT NOT NULL,
+    pet_gender VARCHAR(255) NOT NULL,
+    pet_type VARCHAR(255) NOT NULL,
+    pet_breed VARCHAR(255) NOT NULL,
+    PRIMARY KEY (pet_id)
+);
+
