742a2610a99e49e4b860351c59fd97e23e599e8b
nicholas-kotsakis <nkotsakis13@gmail.com>
2022-05-07 20:42:25 -0400

    homepage and other cleanup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 from flask_bcrypt import Bcrypt
 from flask import Flask, session, abort, redirect, render_template, request
 from dotenv import load_dotenv
@@ -33,29 +33,45 @@ def index():
 
 @app.route('/browse')
 def browse():
-    all_pets = pet_repository_singleton.get_all_pets()
-    return render_template('browse.html', browse_link=True, data=all_pets)
+    pet_type = request.args.get('pettype')
+    print(pet_type)
+    if(pet_type == 'All'):
+        all_pets = pet_repository_singleton.get_all_pets()
+    else:
+        all_pets = pet_repository_singleton.get_pets_by_type(pet_type)
+
+    return render_template('browse.html', browse_link=True, data=all_pets, query=pet_type)
+
+@app.route('/my_listings')
+def listings():
+    
+    my_pets = pet_repository_singleton.get_my_listings(session['user'].get('user_id'))
+    
+    return render_template('my_listings.html', acct_link=True, data=my_pets)
 
 
 @app.post('/account')
 def account():
-    user_username = request.form.get('user_username', '')
-    user_password = request.form.get('user_password', '')
+    
     first_name = request.form.get('first_name', '')
     last_name = request.form.get('last_name', '')
-    user_age = request.form.get('user_age', type=int)
+    user_age = request.form.get('user_age', 1)
     user_gender = request.form.get('user_gender', '')
     user_email_address = request.form.get('user_email_address', '')
-    created_user = pet_repository_singleton.create_user(
-        user_username, user_password, first_name, last_name, user_age, user_gender, user_email_address)
-    return render_template('account.html', account_link=True, data=created_user)
 
-@app.route('/view_account')
-def view_account(user_id):
-    print(user_id)
+    result = pet_repository_singleton.update_user(
+    session['user'].get('user_id'), first_name, last_name, user_age, user_gender, user_email_address)
+
+    created_user = pet_repository_singleton.get_user(session['user'].get('user_id'))
+
+    return render_template('account.html', acct_link=True, data=created_user)
+
+@app.route('/edit_account')
+def view_account():
+    user_id = session['user'].get('user_id')
     single_user = pet_repository_singleton.get_user(user_id)
     print(single_user)
-    return render_template('account.html', account_link=True, data=single_user)
+    return render_template('account.html', acct_link=True, data=single_user)
 
 
 # @app.route('/petfinder')
@@ -80,7 +96,7 @@ def savepet():
     pet_name = request.form.get('pet_name', '')
     pet_type = request.form.get('pet_type', '')
     pet_gender = request.form.get('pet_gender', '')
-    pet_age = 1
+    pet_age = request.form.get('pet_age')
     pet_breed = request.form.get('pet_breed', '')
     pet_health = request.form.get('pet_health', '')
     pet_training = request.form.get('pet_training', '')
@@ -95,15 +111,20 @@ def savepet():
 
     if image:
         # save the image to the satic folder
-        image.save(os.path.join('./static', image.filename))
+        rnd_num = str(random.randrange(0,100,3))
+        new_filename = str(session['user'].get('user_id')) + '_' + rnd_num + '_' + image.filename
+        image.save(os.path.join('./static', new_filename))
         # write the image file to the database
-        photo = image.filename
+        photo = new_filename
     else:
         photo = ''
 
     created_pet = pet_repository_singleton.create_pet(
         pet_name, pet_age, pet_gender, pet_type, pet_breed, pet_health, pet_training, city, state, pet_about, pet_owner, photo)
-    return render_template('petview.html', post_link=True, data=created_pet)
+    
+    my_pets = pet_repository_singleton.get_my_listings(session['user'].get('user_id'))
+    
+    return render_template('my_listings.html', post_link=True, data=my_pets)
 
 
 @app.post('/postcomment')
@@ -182,7 +203,8 @@ def loginuser():
     session['user'] = {
         'username': username,
         'user_id': existing_user.user_id,
-        'user_email': existing_user.user_email_address
+        'user_email': existing_user.user_email_address,
+        'first_name': existing_user.first_name
     }
     print(session['user'].get('username'))
     return render_template('index.html')
@@ -219,9 +241,29 @@ def reguser():
 
     created_user = pet_repository_singleton.create_user(
         user_username, hashed_password, first_name, last_name, user_age, user_gender, user_email_address)
-    return redirect(f'/browse')
+    return redirect(f'/browse?pettype=All')
 
 @app.route("/external-sources")
 def external_sources():
     # ToDo:  Find a better way check for user being logged in
     return render_template('external-sources.html', ext_link=True)
+
+@app.route("/deletecomment")
+def delete_comment():
+    cid = request.args.get('cid')
+    uid = session['user'].get('user_id')
+    pet_repository_singleton.delete_comment(cid, uid)
+    return redirect('/petview/' + str(session.get('pet_id')))
+
+@app.route("/deactivate")
+def deactivate():
+    #delete all pets
+    results = pet_repository_singleton.get_my_listings(session['user'].get('user_id'))
+    for pet in results:
+        print(pet)
+        pet_repository_singleton.delete_pet(pet.pet_id)
+
+    #delete user
+    pet_repository_singleton.delete_user(session['user'].get('user_id'))
+    session.clear()
+    return render_template('index.html', home_link=True)
--- src/repositories/pet_repository.py
+++ src/repositories/pet_repository.py
@@ -11,6 +11,15 @@ class PetRepository:
         db.session.commit()
         return new_user
 
+    def update_user(self, user_id, first_name, last_name, user_age, user_gender, user_email_address):
+        #new_user = User(first_name=first_name, last_name=last_name, user_age=user_age, user_gender=user_gender, user_email_address=user_email_address)
+        upd = update(User)
+        upd = upd.values(first_name=first_name, last_name=last_name, user_age=user_age, user_gender=user_gender, user_email_address=user_email_address)
+        upd = upd.where(User.user_id == user_id)
+        db.session.execute(upd)
+        db.session.commit()
+        return 1
+
     def create_pet(self, pet_name, pet_age, pet_gender, pet_type, pet_breed, pet_health, pet_training, city, state, pet_about, pet_owner, photo):
         new_pet = Pet(pet_name=pet_name, pet_age=pet_age, pet_gender=pet_gender, pet_type=pet_type, pet_breed=pet_breed, pet_health=pet_health, pet_training=pet_training, city=city, state=state, pet_about=pet_about, pet_owner=pet_owner, photo=photo)
         db.session.add(new_pet)
@@ -39,22 +48,55 @@ class PetRepository:
         all_pets = Pet.query.all()
         return all_pets
 
+    def get_pets_by_type(self, pet_type):
+        pets = db.session.query(Pet).filter(Pet.pet_type == pet_type)
+        return pets
+
+    def get_my_listings(self, user_id):
+        results = db.session.query(Pet).filter(Pet.pet_owner == user_id)
+        return results
+
     def get_pet(self, pet_id):
         #result = db.session.query(Pet).filter_by(Pet.pet_id == 7)
-        result = Pet.query.get_or_404(pet_id)
+        #result = Pet.query.get_or_404(pet_id)
+        result = db.session.query(Pet, User).filter(Pet.pet_id == pet_id, Pet.pet_owner == User.user_id).all()
         return result
 
     def get_comments(self, pet_id):
         result = db.session.query(Comment, User).filter(Comment.post_id == pet_id, Comment.author_id == User.user_id).all()
-        print(result)
+        return result
+
+    def get_comment_by_id(self, comment_id):
+        result = Comment.query.get_or_404(comment_id)
         return result
 
     def delete_pet(self,pet_id):
+        
+        #get all comments for pet and delete
+        dele = delete(Comment).where(Comment.post_id == pet_id)
+        db.session.execute(dele)
+        #db.session.commit()
 
         single_pet = Pet.query.get_or_404(pet_id)
         os.remove(os.path.join('./static/' + single_pet.photo))
 
         dele = delete(Pet).where(Pet.pet_id == pet_id)
+        db.session.execute(dele)
+        db.session.commit()
+        return 1
+
+    def delete_comment(self,comment_id,user_id):
+        comment = pet_repository_singleton.get_comment_by_id(comment_id)
+        if(comment.author_id == user_id):
+            dele = delete(Comment).where(Comment.comment_id == comment_id)
+            db.session.execute(dele)
+            db.session.commit()
+            return 1
+        else:
+            return 0
+
+    def delete_user(self, user_id):
+        dele = delete(User).where(User.user_id == user_id)
         db.session.execute(dele)
         db.session.commit()
         return 1
