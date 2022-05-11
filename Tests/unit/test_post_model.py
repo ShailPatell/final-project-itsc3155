1c7866d1c21dc7230f4140dcf3222f2746170254
Asim Siddique <asim.siddiqm@gmail.com>
2022-05-10 22:17:49 -0400

    adding post init

---
+++ Tests/unit/test_post_model.py
@@ -0,0 +1,19 @@
+from models import Pet
+
+def test_user_model():
+    post = Pet('Leo', 4,'Male','Dog','Husky',25,'Healthy','Trained','Charlotte','North Carolina','He is a goodest boy', 1, '1243')
+
+    assert post.pet_name == 'Leo'
+    assert post.pet_age == 4
+    assert post.pet_gender == 'Male'
+    assert post.pet_type == 'Dog'
+    assert post.pet_breed == 'Husky'
+    assert post.pet_health == 'Healthy'
+    assert post.pet_training == 'Trained'
+    assert post.city == 'Charlotte'
+    assert post.state == 'North Carolina'
+    assert post.pet_about == 'He is a goodest boy'
+    assert post.pet_owner == 1
+    assert post.photo == '1234'
+
+    
