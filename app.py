400c508754c011b12e85be6092c33cb98b0caf35
Asim Siddique <asim.siddiqm@gmail.com>
2022-05-11 01:41:45 -0400

    adding test.db and fixing tests

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 
 db = SQLAlchemy()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PET_DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PET_DATABASE_URL', 'sqlite:///test.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv('SECRET_KEY', 'abc123')
 
 bcrypt = Bcrypt(app)
 db.init_app(app)
